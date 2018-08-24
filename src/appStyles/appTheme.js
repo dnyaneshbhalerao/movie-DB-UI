bef33b812bfb853458ce3d6a6a5d9e7e99b6fc78
Dnyaneshwar Ratan Bhalerao <dnyaneshrbhalerao@gmail.com>
2018-08-24 14:30:36 +0530

    Added material-ui setup (#3)

---
+++ src/appStyles/appTheme.js
@@ -0,0 +1,31 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: {
+      main: '#f44336',
+    },
+  },
+  typography: {
+    fontSize: 16,
+    typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+  }
+});
+
+export default theme
--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,7 @@ module.exports = {
         }
       },{
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
